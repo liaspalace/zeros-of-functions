1dbe61d8207f37f3c70fbc629e49d28318edec4d
GiuliaMattedi <giuliamattedi22@gmail.com>
2026-05-30 10:07:02 -0300

    Printing messages and error precision

--- methods.py
+++ methods.py
@@ -1,6 +1,6 @@
 import math
 
-def get_dict()-> dict:
+def get_dict(x, y)-> dict:
     return{
         "x": x,
         "y": y,
@@ -9,16 +9,16 @@ def get_dict()-> dict:
         "cos": math.cos,
         "tan": math.tan,
         "pi": math.pi
-    }
+    } 
 
-def get_interval_length(intervalo: str) -> int:
-    return len(intervalo)
+def get_error_length(erro: float) -> int:
+    return len(str(erro).split(".")[1]) 
 
 def get_error(intervalo: list[float]) -> float: 
     return (intervalo[1] - intervalo[0])/2 #TODO: colocar casas decimais no print do resultado
 
-def get_resultado_da_funcao(funcao:str):
-    return eval(funcao, get_dict())
+def get_resultado_da_funcao(funcao:str, x: float, y: float):
+    return eval(funcao, get_dict(x, y))
 
 def get_midpoint(intervalo: list[float]) -> float:
     return (intervalo[0] + intervalo[1]) / 2
@@ -33,32 +33,29 @@ def bissecao(funcao:str, intervalo: list[float], erro_maximo: float):
         fm = get_resultado_da_funcao(funcao, ponto_medio)
         fb = get_resultado_da_funcao(funcao, b)
 
+        print(f"Consideremos os intervalos:")
+        print(
+            f"[{a:.2f}; {ponto_medio:.2f}] e [{ponto_medio:.2f}; {b:.2f}]"
+        )
+        print(f"f({a:.2f}) = {fa:.2f}")
+        print(f"f({ponto_medio:.2f}) = {fm:.2f}")
+        print(f"f({b:.2f}) = {fb:.2f}")
+
         if (fa < 0 and fm > 0) or (fa > 0 and fm < 0):
-            print(f"Logo, x0 E {{a}; {ponto_medio}}")
+            print(f"Logo, x0 E [{a:.2f}; {ponto_medio:.2f}]")
             intervalo[1] = ponto_medio
             
         elif (fm < 0 and fb > 0) or (fm > 0 and fb < 0):
+            print(f"Logo, x0 E [{ponto_medio:.2f}; {b:.2f}]")
             intervalo[0] = ponto_medio
 
         estimativa = get_midpoint(intervalo)
-        print(f"x0 pertence {estimativa}")
-        print(f"|e| <= {get_error(intervalo)}")
-    return get_midpoint(intervalo)
 
-def bissecao_x(funcao:str, intervalo: list[int], precisao: int):
-    a = intervalo[0]
-    b = intervalo[1]
-    erro = get_error(intervalo)
-    resultado = get_resultado_da_funcao(funcao)
-    while erro > precisao:
-        if resultado == 0:
-            return a
-        elif resultado * get_resultado_da_funcao(funcao.replace("x", str(a))) < 0:
-            b = (a + b) / 2
-        else:
-            a = (a + b) / 2
-        erro = get_error([a, b])
-        resultado = get_resultado_da_funcao(funcao.replace("x", str((a + b) / 2)))
-    return (a + b) / 2
+        print("Assim, uma nova estimava é:")
+        print(f"x0 pertence {estimativa}")
+        print(f"|e| <= {get_error(intervalo):.{get_error_length(get_error(intervalo))}f}")
+    return get_midpoint(intervalo), get_error(intervalo)
+
+
 
 
