c09d2f9cc3fcdedfccf150e1fb9549defb51d86d
Grazi Studies <grazistudies@grazielle.almeida-HVQNH66H00>
2026-06-01 19:27:31 -0300

    CHange variable name for better readability

--- methods.py
+++ methods.py
@@ -15,7 +15,7 @@ def calcular_erro(intervalo: list[float]) -> float:
     return (intervalo[1] - intervalo[0]) / 2 
 
 def calcular_resultado_da_funcao(funcao: str, x: float):
-    return eval(funcao, {"__builtins__": None}, get_dict(x))
+    return eval(funcao, {"__builtins__": None}, criar_dicionario(x))
 
 def calcular_ponto_medio(intervalo: list[float]) -> float:
     return (intervalo[0] + intervalo[1]) / 2
@@ -42,9 +42,9 @@ def bissecao(funcao: str, intervalo: list[float], erro_maximo: float):
         print("\n")
         print(f"Consideremos os intervalos:")
         print(f"[{a:.4f}; {ponto_medio:.4f}] e [{ponto_medio:.4f}; {b:.4f}]")
-        print(f"f({a:.4f}) ≈ {fa:.4f} ({get_sinal(fa)})")
-        print(f"f({ponto_medio:.4f}) ≈ {fm:.4f} ({get_sinal(fm)})")
-        print(f"f({b:.4f}) ≈ {fb:.4f} ({get_sinal(fb)})")
+        print(f"f({a:.4f}) ≈ {fa:.4f} ({calcular_sinal(fa)})")
+        print(f"f({ponto_medio:.4f}) ≈ {fm:.4f} ({calcular_sinal(fm)})")
+        print(f"f({b:.4f}) ≈ {fb:.4f} ({calcular_sinal(fb)})")
 
         if fa * fm < 0:
             print(f"Logo, x0 ∈ [{a:.4f}; {ponto_medio:.4f}]")
